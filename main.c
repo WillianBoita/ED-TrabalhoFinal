df6da890723227ce19aeccd84b125574c13ef5df
WillianBoita <wkboita@gmail.com>
2026-06-15 13:49:51 -0300

    Criando e alocando dados do usuário

--- main.c
+++ main.c
@@ -3,6 +3,44 @@
 #include <stdlib.h>
 #include "structs.h"
 
-int main() {
+void cadastro(char opcao, ArvoreLivro *rootLivro, ArvoreUsuario *rootUsuario) {
+  if (opcao == '0') return;
+  if (opcao == '1') {
+    cadastrarLivro(rootLivro);
+  }
+  if (opcao == '2') {
+    cadastrarUsuario(rootUsuario);
+  }
+}
 
+void cadastrarUsuario(ArvoreUsuario *rootUsuario){
+  Usuario *novoUsuario = (Usuario*) malloc(sizeof(Usuario));
+  char nome[20], email[20];
+
+  printf("Informe o nome do usuário: ");
+  scanf("%s", nome);
+
+  printf("Informe o email do usuário: ");
+  scanf("%s", email);
+
+  novoUsuario->nome = nome;
+  novoUsuario->email = email;
+
+  printf("Nome: %s\n", novoUsuario->nome);
+  printf("Email: %s\n", novoUsuario->email);
+}
+
+void cadastrarLivro(ArvoreLivro *rootLivro){
+
+}
+
+int main() {
+  ArvoreUsuario *usuarios;
+  ArvoreLivro *livros;
+  char opcao;
+
+  scanf("%c", &opcao);
+  cadastro(opcao, livros, usuarios);
+
+  return 0;
 }
--- structs.h
+++ structs.h
@@ -14,20 +14,29 @@ typedef struct {
 
 typedef struct {
   Livro livro;
-  struct Arvore *left, *right;
-} Arvore;
+  struct ArvoreLivro *left, *right;
+} ArvoreLivro;
 
-void cadastro(char opcao, Arvore *root);
+typedef struct {
+  Usuario usuario;
+  struct ArvoreUsuario *left, *right;
+} ArvoreUsuario;
 
-void consulta(char opcao, Arvore *root);
+void cadastro(char opcao, ArvoreLivro *rootLivro, ArvoreUsuario *rootUsuario);
 
-void atualizacao(char opcao, Arvore *root);
+void cadastrarUsuario(ArvoreUsuario *rootUsuario);
 
-void exclusao(char opcao, Arvore *root);
+void cadastrarLivro(ArvoreLivro *rootLivro);
 
-void emprestimo(int codigo, char *email, Arvore *root);
+void consulta(char opcao, ArvoreLivro *root);
 
-void devolucao(int codigo, Arvore *root);
+void atualizacao(char opcao, ArvoreLivro *root);
+
+void exclusao(char opcao, ArvoreLivro *root);
+
+void emprestimo(int codigo, char *email, ArvoreLivro *root);
+
+void devolucao(int codigo, ArvoreLivro *root);
 
 void sair();
 
